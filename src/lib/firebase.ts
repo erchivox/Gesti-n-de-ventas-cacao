b97ad4ae0076ef6ca4199666066acc05e6c3a850
Trino <trinitocarrizo365@gmail.com>
2026-05-12 21:56:16 -0400

    feat: implementar variables de entorno y ocultar claves de API bien

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,22 +3,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// 1. Reemplaza estos valores con la configuración de tu proyecto en Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCewI0aE0XIh9esswRzcRKX0Nd0fikoSxI",
-  authDomain: "gestion-de-ventas-fcdd5.firebaseapp.com",
-  projectId: "gestion-de-ventas-fcdd5",
-  storageBucket: "gestion-de-ventas-fcdd5.firebasestorage.app",
-  messagingSenderId: "415358785064",
-  appId: "1:415358785064:web:018fe0770b3f037fe9c71d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializar Firestore
-const db = getFirestore(app); // Ya no tiene 'export' aquí
-
-export { db }; // Exportación con nombre explícita al final
-// Inicializar Firestore
-//export const db = getFirestore(app);
+export const db = getFirestore(app);
